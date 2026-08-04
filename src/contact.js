2bbad3c0263c45065728da25592f49568498fe8e
HFSaruei <lcristobalbs@gmail.com>
2026-08-04 11:48:43 -0400

    Fix styling fissues and add classes to DOM nodes

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,33 @@
 
 export default function createPage() {
-        const contactContainer = document.createElement("div");
+    const contactContainer = document.createElement("div");
+    contactContainer.classList.add("form-container");
     const contactTitle = document.createElement("h2");
+    contactTitle.classList.add("form-title");
     contactTitle.textContent = "Contact us";
     const contactForm = document.createElement("form");
     const formName = document.createElement("input");
+    formName.classList.add("form-input");
     const nameLabel = document.createElement("label");
+    nameLabel.classList.add("form-label");
     nameLabel.htmlFor = "formName";
     nameLabel.textContent = "Name: ";
     formName.id = "formName";
     formName.required = true;
     formName.name = "clientName";
     const formMail = document.createElement("input");
+    formMail.classList.add("form-input");
     const mailLabel = document.createElement("label");
+    mailLabel.classList.add("form-label");
     mailLabel.textContent = "E-Mail: ";
     mailLabel.htmlFor = "mailLabel";
     formMail.required = true;
     formMail.id = "mailLabel";
     formMail.name = "clientMail";
     const formReason = document.createElement("select");
+    formReason.classList.add("form-input");
     const reasonLabel = document.createElement("label");
+    reasonLabel.classList.add("form-label");
     reasonLabel.textContent = "What is your concern?: ";
     reasonLabel.htmlFor = "clientReason";
     formReason.id = "clientReason";
@@ -34,10 +42,14 @@ export default function createPage() {
     option3.value = "complaint";
     option3.textContent = "Complaint";
     const messageLabel = document.createElement("label");
+    messageLabel.classList.add("form-label");
     messageLabel.htmlFor = "formMessage";
     const formMessage = document.createElement("textarea");
-    messageLabel.textContent = "Tell us what's wrong: ";
+    messageLabel.textContent = "Additional Information: ";
     formMessage.id = "formMessage"
+    formMessage.placeholder = "What's wrong?";
+    formMessage.rows = 5;
+    formMessage.cols = 50;
     formMessage.name = "userMessage";
 
     formReason.append(option1, option2, option3);
